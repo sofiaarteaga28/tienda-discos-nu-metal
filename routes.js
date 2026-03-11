d2121c63792f624fe04cc038ff160eb1d76e72f3
gato <165303591+sofiaarteaga28@users.noreply.github.com>
2026-03-11 16:41:52 -0600

    Delete routes.js

--- routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { obtenerDiscos, crearDisco } = require('../controllers/discosController');
-
-const router = express.Router();
-
-router.get('/', obtenerDiscos);
-router.post('/', crearDisco);
-
-module.exports = router;
